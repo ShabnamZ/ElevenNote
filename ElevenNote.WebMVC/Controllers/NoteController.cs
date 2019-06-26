a32d85b8ef938495b89dd33601e028282fcff655
ShabnamZ <shabnam.ziyovuddinova@gmail.com>
2019-06-26 17:05:38 -0400

    Note Detail Model

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -20,12 +20,6 @@ namespace ElevenNote.WebMVC.Controllers
             return View(model);
         }
 
-        private NoteService CreateNoteService()
-        {
-            var userId = Guid.Parse(User.Identity.GetUserId());
-            var service = new NoteService(userId);
-            return service;
-        }
 
         //GET method
         public ActionResult Create()
@@ -51,6 +45,20 @@ namespace ElevenNote.WebMVC.Controllers
             ModelState.AddModelError("", "Note could not be created.");
 
             return View(model);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var svc = CreateNoteService();
+            var model = svc.GetNoteById(id);
+
+            return View(model);
+        }
+        private NoteService CreateNoteService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userId);
+            return service;
         }
     }
 }
